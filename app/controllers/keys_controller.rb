e9330be3ab33962c812f4f8989208120b638c362
DanielRusinov <daniel.rusinov00@gmail.com>
2018-01-13 21:41:33 +0200

    message

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -1,6 +1,8 @@
 require 'prime'
+require "messages_controller.rb"
 
 class KeysController < ApplicationController
+
   before_action :set_key, only: [:show, :edit, :update, :destroy]
 
 
@@ -28,6 +30,66 @@ class KeysController < ApplicationController
       render json: {id: rsa.id}
     end
   end
+
+class RSA
+	def initialize n, e, d
+		@common = n
+		@public = e
+		@private = d
+	end
+   
+	def n
+		return @common
+	end
+   
+	def e
+		return @public
+	end
+   
+	def d
+		return @private
+	end
+   
+	def self.new_key
+
+		p = Random.new.rand(1..99)
+		q = Random.new.rand(1..99)
+
+		until Prime.prime?(p) == true do
+			p % 2 ? p += 1 : p += 2
+		end
+
+		until Prime.prime?(q) == true do
+			q % 2 ? q += 1 : q += 2
+		end
+
+		n = p * q
+
+		lcm = (p-1).lcm(q-1)
+
+		e = 1
+		while true do
+			e = Random.new.rand(1..lcm)
+			if e.gcd(lcm) == 1
+				break
+			end
+	
+		end
+
+		d = 1
+		while true do
+			if(d * e) %lcm == 1
+				break
+			end
+			d += 1
+			
+		end
+
+		arr = [n, e, d]
+		return arr
+
+   end
+end
 
 
   def edit
@@ -59,74 +121,5 @@ class KeysController < ApplicationController
     def key_params
       params.require(:key).permit(:n, :e, :d)
     end
-
-	class RSA
-   def initialize n, e, d
-	@common = n
-	@public = e
-	@private = d
-   end
-   
-   def n
-	return @common
-   end
-   
-   def e
-	return @public
-   end
-   
-   def d
-	return @private
-   end
-   
-   def self.new_key
-
-	p = Random.new.rand(1..99)
-	q = Random.new.rand(1..99)
-
-	until Prime.prime?(p) == true do
-		p % 2 ? p += 1 : p += 2
-	end
-
-	until Prime.prime?(q) == true do
-		q % 2 ? q += 1 : q += 2
-	end
-
-	n = p * q
-
-	lcm = (p-1).lcm(q-1)
-
-	e = 1
-	while true do
-		e = Random.new.rand(1..lcm)
-		if e.gcd(lcm) == 1
-			break
-		end
-	
-	end
-
-	d = 1
-	while true do
-		if(d * e) %lcm == 1
-			break
-		end
-		d += 1
-			
-	end
-
-	arr = [n, e, d]
-	return arr
-
-   end
-   
-   def encrypt message
-	return (message.chars.map {|m| m.ord ** @public %  @common }).join(',')
-   end
-   
-   def decrypt message
-	return (message.split(",").map {|m| (m.to_i ** @private %  @common).chr }).join('')
-   end 
-end
-
 
 end
